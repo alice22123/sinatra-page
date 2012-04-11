9e667a6d1ba3edef8f1295565cea861e1d12d2c6
Your Name <alice22123@hotmail.com>
2012-04-11 14:24:26 +0100

    sinatra and heroku, two great tastes

---
+++ server.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require "sinatra/reloader"
+get '/' do 
+  erb :index
+end
+
+get '/page1' do
+  erb :page1
+end 
+
+get '/page2' do 
+  erb :page2
+end
+
+get '/page3' do
+  erb :page3
+end
